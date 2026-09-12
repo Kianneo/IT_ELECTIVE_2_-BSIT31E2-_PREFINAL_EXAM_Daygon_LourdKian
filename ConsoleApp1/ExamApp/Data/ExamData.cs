6880574b6f4823778ead86bf537976b346ad6efd
Kianneo <lourdkianhayden@gmail.com>
2026-09-12 17:05:44 +0800

    ExamData

---
+++ ConsoleApp1/ExamApp/Data/ExamData.cs
@@ -0,0 +1,15 @@
+﻿using PrefinalExam.Models;
+
+namespace PrefinalExam.Data
+{
+
+    public static class ExamData
+    {
+        public static List<QuestionAnswer> Questions { get; } = new();
+
+        static ExamData()
+        {
+    
+        }
+    }
+}
