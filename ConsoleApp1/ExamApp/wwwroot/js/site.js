fff3146322a9e8ae8c3816891df1d2411b8bfcbf
Kianneo <lourdkianhayden@gmail.com>
2026-09-12 17:10:18 +0800

    Js

--- ConsoleApp1/ExamApp/wwwroot/js/site.js
+++ ConsoleApp1/ExamApp/wwwroot/js/site.js
@@ -1,4 +1,8 @@
-﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
-
-// Write your JavaScript code.
+﻿document.querySelectorAll(".flip-card").forEach((card) => {
+    card.addEventListener("keydown", (e) => {
+        if (e.key === "Enter" || e.key === " ") {
+            e.preventDefault();
+            card.classList.toggle("flipped");
+        }
+    });
+});
